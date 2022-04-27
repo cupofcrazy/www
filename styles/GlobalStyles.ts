1b6b4269e60cf9edb2b1bdb91a2476cac1fd77a1
Tobi Balogun <tobybarlhogun@gmail.com>
2022-04-27 09:18:08 -0700

    update

--- styles/GlobalStyles.ts
+++ styles/GlobalStyles.ts
@@ -5,10 +5,10 @@ export const GlobalStyles = createGlobalStyle`
 
   /* Fonts */
   @font-face {
-    font-family: "Lars";
+    font-family: "Graphik";
     font-weight: 300;
-    src: url('/fonts/Lars-Light.woff') format('woff'),
-    url('/fonts/Lars-Light.woff2') format('woff2');
+    src: url('/fonts/Graphik-Regular.woff') format('woff'),
+    url('/fonts/Graphik-Regular.woff2') format('woff2');
   }
   
   /* Box sizing rules */
@@ -60,7 +60,7 @@ export const GlobalStyles = createGlobalStyle`
     /* scroll-behavior: smooth; */
     font-size: 14px;
     letter-spacing: 0.25px;
-    font-family: 'Graphik', 'Lars', Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    font-family: 'Graphik', Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 
     @media ${mq.md} {
